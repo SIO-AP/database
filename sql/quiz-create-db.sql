07b1d98c7113c2fd033ed88bd237103b850ea652
SohKey <45180021+SohKey@users.noreply.github.com>
2022-12-16 14:11:46 +0100

    update sql

--- sql/quiz-create-db.sql
+++ sql/quiz-create-db.sql
@@ -1,28 +1,28 @@
-#databases list on instance
-show databases;
-#quiz_db database creation
-CREATE DATABASE quiz_db;
-USE quiz_db;
+DROP TABLE if exists game_question cascade;
+DROP TABLE if exists party cascade;
+DROP TABLE if exists answer cascade;
+DROP TABLE if exists question cascade;
+DROP TABLE if exists player cascade;
 
 CREATE TABLE player (
-	id_player int NOT NULL AUTO_INCREMENT,
-    name_player varchar(20),
-    score_player int,
+    id_player INTEGER NOT NULL AUTO_INCREMENT,
+    name_player VARCHAR(20),
+    score_player INTEGER,
     PRIMARY KEY(id_player)
 );
 
 CREATE TABLE party (
-	id_party	int NOT NULL AUTO_INCREMENT,
-    party_name	varchar(20),
-    id_player	int,
+    id_party INTEGER NOT NULL AUTO_INCREMENT,
+    name_party VARCHAR(20),
+    id_player INTEGER,
     PRIMARY KEY(id_party),
     foreign key (id_player) references player(id_player)
-);
+   );
 
 CREATE TABLE question(
     id_question        INTEGER,
     desc_question    VARCHAR(255),
-    difficulte int,
+    difficulte INTEGER,
     PRIMARY KEY (id_question)
 );
 
@@ -39,10 +39,3 @@ CREATE TABLE answer (
     id_question        INTEGER,
     FOREIGN KEY (id_question) REFERENCES question(id_question)
 );
-
-#quiz_dba owner creation
-#CREATE USER 'quiz_dba'@'localhost' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON quiz_db.* TO quiz_dba@localhost;
-FLUSH PRIVILEGES;
-#tables list on quiz_db
-show tables;
